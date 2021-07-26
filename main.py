c54d9d7cd0c1cfd41857bbecd54e983d92537128
Nitu Liviu Daniel <siszak01@gmail.com>
2021-07-26 11:07:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#1 problem
+
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+my_list.sort()
+print (my_list)
+
+#2 problem
+
+my_list.sort(reverse=True)
+print (my_list)
+
+#3
+my_sliced_list = my_list[::2]
+print (my_sliced_list)
+
+#4
+my_sliced_list2 = my_list[1::2]
+print (my_sliced_list2)
+
+#5
+my_sliced_list3 = my_list[1::3]
+print (my_sliced_list3)
+
